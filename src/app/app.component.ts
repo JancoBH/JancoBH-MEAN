c177617b074caed78d46760e85287056e07265a1
Janco <jancoboscan@gmail.com>
2019-09-13 17:51:19 -0300

    update angular + mini responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import {Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
+import {ChangeDetectorRef, Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
 import {NavigationEnd, Router} from '@angular/router';
 import {isPlatformBrowser} from '@angular/common';
+import {MediaMatcher} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
@@ -8,14 +9,23 @@ import {isPlatformBrowser} from '@angular/common';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+
+  mobileQuery: MediaQueryList;
   title = 'JancoBH-MEAN';
 
   private isBrowser: boolean = isPlatformBrowser(this.platformId);
+  private _mobileQueryListener: () => void;
 
   constructor(
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher,
     private router: Router,
     @Inject(PLATFORM_ID) private platformId: any
-  ) {}
+  ) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
 
   ngOnInit() {
     this.router.events.subscribe((evt) => {
